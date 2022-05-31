dea8de6efdc5645c6caa44a33f547df5beee6c90
Tamia Quiyara <mia.mbuckingworth@gmail.com>
2022-05-31 15:58:44 +0200

    fourth commit

---
+++ apple_packet_dom.js
@@ -0,0 +1,14 @@
+const calculateBtn = document.querySelector(".calculate");
+const numOfPackets = document.querySelector(".num-packets");
+const costPerApple = document.querySelector(".pp-apple");
+const costPerPacket = document.querySelector(".pp-packet");
+const recommendedPackets = document.querySelector(".recommended-packets");
+
+calculateBtn.addEventListener("click", function(){
+    alert(calculateBtn);
+    // numOfPackets.innerHTML = 
+    // costPerApple.innerHTML =
+    // costPerPacket.innerHTML = 
+    // recommendedPackets.innerHTML = 
+
+});
--- tests/apple_packet_price.test.js
+++ tests/apple_packet_price.test.js
@@ -1,10 +1,34 @@
 describe('The apple packet price planner app', function() {
 
-	it ('should take in the cost price for a box', function() {
+	it ('should take in the cost price for a box of apples', function() {
 
 		const applePacketPr = ApplePacketPr();
 
-		assert.equal(5, applePacketPr());
+		assert.equal(10, applePacketPr(5));
+
+	});
+
+	it ('should take in the number of apples in a box', function() {
+
+		const applePacketPr = ApplePacketPr();
+
+		assert.equal(5, applePacketPr(5));
+
+	});
+
+	it ('should take in the profit in percentage', function() {
+
+		const applePacketPr = ApplePacketPr();
+
+		assert.equal(5, applePacketPr(5));
+
+	});
+
+	it ('should take in the size of the packet', function() {
+
+		const applePacketPr = ApplePacketPr();
+
+		assert.equal(5, applePacketPr(5));
 
 	});
 });
